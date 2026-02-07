0c1514933f448105f1019a42d2ee067a640a3127
Hashem <hhosseinzadeh@uco.edu>
2026-02-06 19:09:00 -0600

    lightbox for portfolio added

---
+++ hhb/js/lightbox.js
@@ -0,0 +1,14 @@
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+document.querySelectorAll(".masonry img").forEach(img => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightbox.classList.add('active');
+    })
+})
+
+lightbox.addEventListener('click', () => {
+    lightbox.classList.remove('active');
+    lightboxImg.src = '';
+})
