aded97d5b0eea4eeda3787585b41a37e2db0dd5c
Hashem <hhosseinzadeh@uco.edu>
2026-02-12 21:42:47 -0600

    updates main

--- hhb/js/parallax.js
+++ hhb/js/parallax.js
@@ -1,5 +1,6 @@
 const layers = document.querySelectorAll(".parallax_layer");
 let lastScrollY = 0;
+const isMobile = window.innerWidth < 900;
 
 window.addEventListener("scroll", () => {
   lastScrollY = window.scrollY;
@@ -8,7 +9,10 @@ window.addEventListener("scroll", () => {
 
 function updateParallax() {
   layers.forEach(layer => {
-    const speed = Number(layer.dataset.speed);
+    let speed = Number(layer.dataset.speed);
+    if (isMobile) {
+      speed *= 0.5;
+    }
     layer.style.transform = `translateY(${-lastScrollY * speed}px)`;
   });
 }
